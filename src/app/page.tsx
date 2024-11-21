1e3073498bb6aa1308671111cbefc7a08aeeff96
Shehbaz Ali <shahmainaqpa@gmail.com>
2024-11-22 01:57:22 +0500

    fix: deployment issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Products from "./products/page";
 
 const Page = () => {
   return (
     <>
-      <Products />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Products />
+      </Suspense>
     </>
   );
 };
